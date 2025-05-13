bd946a1c8e679cfaf2c8680769a3c7095b69159d
JustaMushroom <ariaramshaw@gmail.com>
2025-05-13 02:11:57 +0100

    Add basic region info

---
+++ worlds/robobeat/Regions.py
@@ -0,0 +1,12 @@
+from typing import Dict, List
+
+
+all_regions: Dict[str, List[str]] = {
+    "The HUB": ["Communications", "The Old Passage"],
+    "Communications": [],
+    "The Old Passage": ["The Gallery"],
+    "The Gallery": ["The Ruins"],
+    "The Ruins": ["The Citadel"],
+    "The Citadel": ["The Machine"],
+    "The Machine": []
+}

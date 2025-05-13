e6736a6f2168bac245976f7a9300f30e8cd0f449
JustaMushroom <ariaramshaw@gmail.com>
2025-05-13 12:39:48 +0100

    Update the world options

--- worlds/robobeat/Options.py
+++ worlds/robobeat/Options.py
@@ -1,9 +1,15 @@
 from Options import Toggle, Choice, DeathLinkMixin, StartInventoryPool, PerGameCommonOptions, DefaultOnToggle
 
-class RandomizeStartingLoadouts(Toggle):
-    """Randomizes the 4 starting weapons (Revolver, Flux, Contact, Ping Pong) + the 2 starting utilities (Teleport, Lightning Strike)"""
+class RandomizeStartingLoadout(Toggle):
+    """Randomizes the 4 starting weapons (Revolver, Flux, Contact, Ping Pong) + the 2 starting utilities (Teleport, Lightning Strike)
+    Currently not implemented"""
     display_name = "Randomize Starting Loadout"
+    default = False
+
+class Blueprintsanity(Toggle):
+    """Add blueprint unlocks to the pool, doubles the number of blueprint checks
+    Currently not implemented"""
 
 @dataclass
-class RobobeatOptions(PerGameCommonOptions):
-    pass
+class RobobeatOptions(DeathLinkMixin, PerGameCommonOptions):
+    randomize_starting_loadout: RandomizeStartingLoadout
